ba4ff2f61f6c74a9c657d222ce1c1b1e0096f7eb
Vanish dhiman <vanishdhiman@192.168.1.3>
2026-02-09 01:51:15 +0530

    added otp verification , console issue

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,17 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, _res, next) => {
+  console.log("🌍 SERVER RECEIVED:", req.method, req.url);
+  next();
+});
+
+
+// app.get("/", (_req, res) => {
+//   res.send("Backend running (TypeScript)");
+// });
+
 app.use("/test", testRoutes);
 app.use("/auth", authRoutes);
-
-app.get("/", (_req, res) => {
-  res.send("Backend running (TypeScript)");
-});
 
 export default app;
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { findUserByEmail } from "../repositories/user.repository.js";
 import { comparePassword } from "../utils/password.js";
+import { generateOtp, hashOtp } from "../utils/otp.js";
+import { createOtpEntry } from "../repositories/otp.repository.js";
 
 const router = Router();
 
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log("🌍 SERVER RECEIVED LOGIN:", req.method, req.url);
+
 
     if (!email || !password) {
       return res.status(400).json({ message: "Email and password required" });
@@ -27,13 +31,26 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
+    console.log("2FA FLAG FROM DB:", user.is_2fa_enabled, typeof user.is_2fa_enabled);
+
     // 🚦 Decision point for 2FA
-    if (user.is_2fa_enabled) {
-      return res.json({
-        otpRequired: true,
-        userId: user.id
-      });
-    }
+  if (user.is_2fa_enabled) {
+  const otp = generateOtp();
+  const otpHash = await hashOtp(otp);
+
+  const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 min
+
+  await createOtpEntry(user.id, otpHash, expiresAt);
+
+  // TEMP: log OTP for POC (REMOVE LATER)
+  console.log("OTP for testing:", otp);
+
+  return res.json({
+    otpRequired: true,
+    message: "OTP generated"
+  });
+}
+
 
     // Non-2FA users (temporary success response)
     return res.json({
